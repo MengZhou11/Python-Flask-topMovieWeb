7b0c348817d9ec0d52ce17519356b216a5865a4c
Meng <61612823+MengZhou11@users.noreply.github.com>
2021-06-15 17:25:38 -0400

    merge

--- testCloud.py
+++ testCloud.py
@@ -1,3 +1,5 @@
+from tkinter import Image
+
 import jieba
 
 from matplotlib import pyplot as plt
@@ -6,3 +8,24 @@ from PIL import image
 import numpy as np
 import sqlite3
 
+con = sqlite3.connect('movie.db')
+cur = con.cursor()
+sql = 'select introduction from top250movie'
+data = cur.execute(sql)
+
+text=""
+for item in data:
+    text = text+item[0]
+
+cut = jieba.cut(text)
+string = ' '.join(cut)
+print(string)
+
+img = Image.open(r'/static/assets/img/hero-img.png')
+img_array = np.array(img)
+wc = WordCloud(
+    background_color='white'
+)
+
+cur.close()
+con.close()
